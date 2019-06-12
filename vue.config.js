76d8fc467600067da7be6aed7c56236c477fefa4
Leoflutter <haibudongshi@gmail.com>
2019-06-12 15:15:20 +0800

    多页面

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-    // publicPath: '/leo-study',
-    
+    pages: {
+        home: {
+            entry: 'src/pages/Home/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'Index Page',
+
+        },
+        about: {
+            entry: 'src/pages/About/main.js',
+            template: 'public/index.html',
+            filename: 'about.html',
+            title: 'About Page'
+        }
+    }
 }
