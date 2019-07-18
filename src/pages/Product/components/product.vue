329751d900b35c7a5e588db077efdbcb9cc5f2c7
Leoflutter <haibudongshi@gmail.com>
2019-07-18 11:17:01 +0800

    none

--- src/pages/Product/components/product.vue
+++ src/pages/Product/components/product.vue
@@ -1,6 +1,47 @@
 <template>
-  <div class="product">
-    <label for>This is product page.</label>
+  <div id="product">
+    <main id="main">
+      <header id="header">
+        <nav id="nav-bar">
+          <ul>
+            <li class="nav-link"></li>
+            <li class="nav-link"></li>
+            <li class="nav-link"></li>
+          </ul>
+        </nav>
+      </header>
+      <div id="content">
+        <h2>Handcrafted, home-made masterpieces</h2>
+        <form action id="form">
+          <input type="email" name="email" id="email" placeholder="Enter your email address" />
+          <input type="submit" value="Submit" id="submit" />
+        </form>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, mollitia nostrum. Quae doloribus accusantium repellendus molestiae consequatur tempora aut, placeat ullam sed officia, facere rerum ad modi illo totam saepe.
+        </p>
+        <video src="https://www.youtube.com/watch?v=HaeYXG92J4E" id="video">Video</video>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -9,4 +50,15 @@ export default {};
 </script>
 
 <style>
+header {
+  background: #ffffff;
+  position: sticky;
+  top: 0px;
+}
+
+@media screen and (max-width: 520px) {
+  header {
+    background: #000;
+  }
+}
 </style>
