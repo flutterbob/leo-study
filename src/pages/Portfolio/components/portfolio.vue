01a7c3b0e4977dd0bd1b63ebb78da5f9e0ac8846
Leoflutter <haibudongshi@gmail.com>
2019-07-28 15:37:11 +0800

    Sunday Start

--- src/pages/Portfolio/components/portfolio.vue
+++ src/pages/Portfolio/components/portfolio.vue
@@ -18,11 +18,13 @@
       </header>
       <div class="content">
         <section id="welcome-section">
-          <h1>Hi! I'm Leo</h1>
+          <h1>Hey! I'm Leo</h1>
+          <h2>
+            <em>a web developer</em>
+          </h2>
         </section>
         <section id="projects">
           <div class="project-title">
-            <!-- TODO -->
             <a href="#">link</a>
           </div>
         </section>
@@ -38,13 +40,20 @@ export default {};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 html,
 body {
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
+  /* height: 100%; */
+  /* width: 100%; */
+}
+
+body {
+  background-color: green;
 }
 
 #app,
@@ -56,7 +65,40 @@ main {
 
 header {
   width: 100%;
-  height: 15vh;
-  background: red;
+  height: 12vh;
+  background: #bc3034;
+  display: flex;
+  position: sticky;
+}
+
+header nav ul {
+  display: flex;
+  list-style-type: none;
+  height: 100%;
+}
+
+header nav ul li a {
+  text-decoration: none;
+  color: #eee;
+}
+
+#welcome-section {
+  background: #202023;
+  height: 88vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#welcome-section h1 {
+  font-size: 3em;
+  color: #eee;
+}
+
+#welcome-section h2 {
+  padding-top: 10px;
+  font-weight: 100;
+  color: red;
 }
 </style>
